f4f18f1aff778ba800561d3018eb719197b9b8fe
PatrikJMelander <patrikjmelander@gmail.com>
2022-02-25 09:30:11 +0100

    added modal to register and login

--- src/app/public/sidebar/public-navbar.component.ts
+++ src/app/public/sidebar/public-navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-public-navbar',
@@ -7,7 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublicNavbarComponent implements OnInit {
 
-  constructor() { }
+  closeResult = '';
+
+  constructor(private modalService: NgbModal) { }
+
+  open(register: any) {
+    this.modalService.open(register, {ariaLabelledBy: 'modal-basic-title'}).result.then((result: any) => {
+      this.closeResult = `Closed with: ${result}`;
+      console.log(register)
+    }, (reason: any) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  onClickRegister(){
+    console.log("test")
+  }
+
+  onClickLogin(data: any) {
+    console.log("Entered Email id : " + data.emailid);
+  }
+
+  goToPlaceTips(){
+    console.log("goToPlaceTips")
+  }
+
+  goToTopList(){
+    console.log("goToTopList")
+  }
+
+  goToMyResult(){
+    console.log("goToMyResult")
+  }
+
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 
   ngOnInit(): void {
   }
